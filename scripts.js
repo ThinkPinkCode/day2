ba0c87be7756f8d6a99aa9b0c044aa4d272ce372
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-27 20:40:41 -0500

    updated styling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 /**
  * Created by melanie.myers on 1/27/17.
  */
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+
+    console.log(now);
+    console.log(seconds);
+}
+
+setInterval(setDate, 1000);
