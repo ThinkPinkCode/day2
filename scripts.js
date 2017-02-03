1e4967d049080f014efb448e9b0a442ce331372c
Melanie Myers <melanie.myers@365smartshop.com>
2017-02-02 20:52:37 -0500

    changed colors of hands

--- scripts.js
+++ scripts.js
@@ -5,16 +5,20 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const dateSection = document.querySelector('.date');
 
 
+function setDay() {
+    const now = new Date();
+    const year = now.getFullYear()
+    dateSection.innerHTML = year;
+}
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-    console.log(seconds);
 
     if (seconds === 0) {
         setMinuteHand();
@@ -26,7 +30,6 @@ function setMinuteHand() {
     const now = new Date();
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
-    console.log(`minutes: ${minutes} minutesDegrees: ${minutesDegrees}`);
 
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
@@ -40,12 +43,12 @@ function setHourHand() {
     const now = new Date();
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + 90;
-    console.log(`hour: ${hour} hourDegrees: ${hourDegrees}`);
 
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 }
 
+setDay();
 setHourHand();
 setMinuteHand();
 setInterval(setDate, 1000);
